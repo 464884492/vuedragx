1f4da09c1bcd70b78550e7d0bb6566193d64534d
464884492 <464884492@qq.com>
2018-05-11 16:11:30 +0800

    移除不需要组件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
